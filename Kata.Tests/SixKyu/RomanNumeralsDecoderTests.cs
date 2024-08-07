b0a9be723b31bc3b8b8f9807093cbfc0cb89e094
Albin <albinronnkvist@protonmail.com>
2024-08-07 12:32:00 +0200

    Add RomanNumeralsDecoder

---
+++ Kata.Tests/SixKyu/RomanNumeralsDecoderTests.cs
@@ -0,0 +1,23 @@
+using FluentAssertions;
+using AlbinRonnkvist.Kata.Solutions.SixKyu;
+
+namespace AlbinRonnkvist.Kata.Tests.SixKyu;
+
+public class RomanNumeralsDecoderTests
+{
+    [Theory]
+	[InlineData(1, "I")]
+	[InlineData(2, "II")]
+	[InlineData(4, "IV")]
+    [InlineData(21, "XXI")]
+	[InlineData(500, "D")]
+	[InlineData(1000, "M")]
+	[InlineData(1954, "MCMLIV")]
+	[InlineData(1990, "MCMXC")]
+	[InlineData(2008, "MMVIII")]
+	[InlineData(2014, "MMXIV")]
+	public void RomanNumeralsDecoder_ShouldReturnExpectedNumber(int expected, string roman)
+	{
+		RomanNumeralsDecoder.Solution(roman).Should().Be(expected);
+	}
+}
